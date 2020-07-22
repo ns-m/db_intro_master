b3d2e0c26ffffdaff53df9b7dc014a49a4535e67
kmv <9629095@gmail.com>
2020-07-22 17:05:42 +0300

    day 2.3 3

--- week_2/my_script_week2.sql
+++ week_2/my_script_week2.sql
@@ -155,8 +155,32 @@
 #    ON A.b_id = B.id
 #  LEFT OUTER JOIN C
 #    ON a.c_id = C.id;
-use store;
-select  count(1) as sale_num from good inner JOIN sale_has_good as salehg on good.id = salehd.good_id
-union
-select category.name as name from good LEFT OUTER JOIN category_has_good as c on good.id = c.good_id
-LEFT OUTER JOIN category on category.id = c.category_id group by name order by sale_num desc;
+#use store;
+#SELECT category.name AS name, COUNT(sale.id) AS sale_num FROM category
+#LEFT OUTER JOIN category_has_good as chg ON category.id = chg.category_id
+#LEFT OUTER JOIN good ON chg.good_id = good.id
+#LEFT OUTER JOIN sale_has_good as salehg ON good.id = salehg.good_id
+#LEFT OUTER JOIN sale ON salehg.sale_id = sale.id GROUP BY category.name order by sale_num desc;
+
+#Задание
+#Выведите список источников, из которых не было клиентов, либо клиенты пришедшие из которых не совершали заказов или отказывались от заказов. 
+#Под клиентами, которые отказывались от заказов, необходимо понимать клиентов, у которых есть заказы, которые на момент выполнения запроса находятся в состоянии 'rejected'. 
+#В запросе необходимо использовать оператор UNION для объединения выборок по разным условиям.
+#Ожидаемый формат результата:
+#+-------------+
+#| source_name |
+#+-------------+
+#| source 1    |
+#| source 2    |
+#| source 3    |...... 
+#NB! 
+#Для проверки существования записей в каких-либо таблицах можно использовать оператор EXISTS в условии WHERE. Например, вывести список категорий, к которым не принадлежат никакие товары можно следующим образом:
+#SELECT c.id, c.name FROM category AS c 
+#  WHERE NOT EXISTS (SELECT * FROM category_has_good AS cg
+#        WHERE cg.category_id = c.id);
+#В запросе для соединения нескольких источников данных операцию соединения можно использовать многократно. Например, для соединения таблиц A, B и C можно использовать запрос вида:
+#SELECT * FROM A
+#  INNER JOIN B
+#    ON A.b_id = B.id
+#  INNER JOIN C
+#    ON a.c_id = C.id;
